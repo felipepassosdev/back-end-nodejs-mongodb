d67595431a5b80653a2f4a504a51d3ac8d6e6a65
Felipe Passos <38237206+felipepassosc@users.noreply.github.com>
2019-10-29 11:10:16 -0300

    mongoose

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
+// STARTING APP
 const app = express()
 
+// STARTING BD
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+    { useNewUrlParser: true }
+)
+
+// FIRST ROUTE
 app.get('/', (req, res) => {
     res.send('How are you? I am fine, thanks')
 })
 
+// LISTENING APP
 app.listen(3001)
+
